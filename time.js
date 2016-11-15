dbff06946e041ca1b11e8a66e2ae58c5d2c4b43e
WinBarries <tbola45@gmail.com>
2016-11-15 17:31:21 -0500

    Update time.js

--- time.js
+++ time.js
@@ -1,3 +1,31 @@
-function Time() {
-	
-}
+	 function Time() {
+	 	var date = new Date();
+	 	var Hour = date.getHours();
+	 	if (Hour < 10) {
+	 		Hour = "0" + Hour;
+	 	}
+	 	if (Hour == 00) {
+	 		Hour = 12
+	 	}
+	 	var Minute = date.getMinutes();
+	 	if (Minute < 10) {
+	 		Minute = "0" + Minute;
+	 	}
+	 	var Second = date.getSeconds();
+	 	if (Second < 10) {
+	 		Second = "0" + Second;
+	 	}
+	 	var meridian = "AM";
+	 	if (Hour > 12 && Minute > 0) {
+	 		Hour = Hour - 12;
+	 		meridian = "PM";
+	 	}
+	 	if (Hour === 0 ) {
+	 		Hour = 12;
+	 	}
+	 	var clockDiv = document.getElementById('clock');
+	 	clockDiv.innerHTML = (Hour + ":" + Minute + ":" + Second + " " + meridian);
+	 	//document.body.innerHTML = (Hour + ":" + Minute + ":" + Second);
+	 	
+	}
+	setInterval (Time, 1000);
